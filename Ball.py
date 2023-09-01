e3d11f23bc6d2c609abd1c60a1fbf0e1a3a8b578
tony-ngo-03 <tonyngo@utexas.edu>
2023-08-31 23:00:51 -0500

    added comments for Paddle and Ball

--- Ball.py
+++ Ball.py
@@ -2,40 +2,59 @@ import pygame
 
 
 class Ball:
-
-    def __init__(self, pos: pygame.Vector2, speed: float, color, radius=15):
+    # constructor of Ball
+    # pre: pos is type Vector2, speed is type float
+    def __init__(self, pos: pygame.Vector2, speed: float, color, radius=15) -> None:
+        # basic information for ball
         self.position = pos
         self.speed = speed
         self.color = color
         self.radius = radius
+        # control movement
         self.move_left = True
         self.move_down = True
-
+        # resetting information
         self.default_pos = pygame.Vector2(pos.x, pos.y)
         self.default_speed = speed
 
-    def reset_ball(self):
+    # resets the ball to the center of the screen while retaining current direction
+    # pre: ball must have gotten off the screen
+    # post: |self.position| changed
+    def reset_ball(self) -> None:
         self.position = pygame.Vector2(self.default_pos.x, self.default_pos.y)
         self.speed = self.default_speed
 
-    def bounce(self, paddle_list: list, this_circle):
+    # detects if the ball has collided with a paddle, and if so reverse horizontal movement
+    # pre: paddle_list is a list of paddles, this_circle != None
+    # post: may or may not change direction of ball
+    def bounce(self, paddle_list: list, this_circle) -> None:
         for paddle in paddle_list:
             if this_circle.colliderect(paddle):
                 self.move_left = not self.move_left
 
-    def move_validation(self):
+    # makes sure the ball does not go off the top and bottom edges of the screen
+    # pre: none
+    # post: |move_down| may change
+    def move_validation(self) -> None:
         if self.position.y + self.radius > 720:
             self.move_down = False
         if self.position.y - self.radius < 0:
             self.move_down = True
 
+    # gives us an indication if the ball goes off the screen horizontally
+    # pre: none
+    # post: return True if we go off the screen horizontally, False otherwise
     def off_screen(self) -> bool:
         if self.position.x + self.radius > 1080:
             return True
         if self.position.x - self.radius < 0:
             return True
+        return False
 
-    def move(self):
+    # moves the ball by |speed|
+    # pre: none
+    # post: |position| changed
+    def move(self) -> None:
         if self.move_left:
             self.position.x -= self.speed
         if not self.move_left:
@@ -46,6 +65,9 @@ class Ball:
             self.position.y -= self.speed
         self.move_validation()
 
+    # draws the ball onto the screen
+    # pre: screen is type surface, paddle_list != None
+    # post: returns None
     def draw(self, screen: pygame.surface, paddle_list: list) -> None:
         this_ball = pygame.draw.circle(screen, self.color, self.position, self.radius)
         self.bounce(paddle_list, this_ball)
--- Paddle.py
+++ Paddle.py
@@ -2,23 +2,34 @@ import pygame
 
 
 class Paddle:
+    # constructor for the Paddle class
+    # pre: pos is type Vector2, speed is type float, is_human True for the player
     def __init__(self, pos: pygame.Vector2, speed: float, color, is_human, width=15, height=100) -> None:
+        # basic information about the paddle
         self.position = pos
         self.speed = speed
         self.color = color
         self.width = width
         self.height = height
         self.rect = pygame.Rect(pos.x, pos.y, width, height)
+        # with player/enemy movement
         self.is_player = is_human
         self.control_enemy = False
 
+        # reset information
         self.default_pos = pygame.Vector2(pos.x, pos.y)
         self.default_speed = speed
 
+    # reverts the current position to the starting
+    # pre: the ball has gone past the paddle
+    # post: returns None, paddle is now at middle of the screen
     def reset_paddle(self):
         self.position = pygame.Vector2(self.default_pos.x, self.default_pos.y)
         self.speed = self.default_speed
 
+    # makes sure that the paddle is not out of bounds
+    # pre: none
+    # post: returns True if paddle are out of bounds, False otherwise
     def position_check(self) -> bool:
         top_of_screen = 0
         bottom_of_screen = 720
@@ -30,19 +41,30 @@ class Paddle:
             return True
         return False
 
+    # moves the paddle |self.speed| vertically upwards
+    # pre: none
+    # post: returns True if paddle is out of bounds, False otherwise
     def move_up(self) -> bool:
         self.position.y -= self.speed
         return self.position_check()
 
+    # moves the paddle |self.speed| vertically downwards
+    # pre: none
+    # post: returns True if paddle is out of bounds, False otherwise
     def move_down(self) -> bool:
         self.position.y += self.speed
         return self.position_check()
 
+    # moves the paddle vertically upwards or downwards depending on input
+    # pre: key is type pygame.key
+    # post: moves the paddle
     def move(self, key: pygame.key):
+        upwards_key = pygame.K_w
+        downwards_key = pygame.K_s
         if self.is_player:
-            if key[pygame.K_w]:
+            if key[upwards_key]:
                 self.move_up()
-            if key[pygame.K_s]:
+            if key[downwards_key]:
                 self.move_down()
         else:
             if self.control_enemy:
@@ -51,10 +73,16 @@ class Paddle:
             else:
                 if self.move_up():
                     self.control_enemy = not self.control_enemy
+        # always remake the rect to see changes.
         self.rect = pygame.Rect(self.position.x, self.position.y, self.width, self.height)
 
+    # draws the paddle onto the screen
+    # pre: screen != None, screen is type surface
+    # post: paddle is drawn onto the screen
     def draw(self, screen: pygame.surface):
         pygame.draw.rect(screen, "white", self.rect)
 
+    # getter method for the position if needed
+    # TODO: get rid if needed
     def get_pos(self) -> pygame.Vector2:
         return self.position
